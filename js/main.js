3147e0c991aab69bbc8de781af7c507f9286a7d8
ImZirix <xxkillerxx.xg@gmail.com>
2024-09-22 12:57:27 +0300

    made a function to take the buttons value and pass it to the operate function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-let num1;
-let num2;
-let op;
+//Declaring variables to display them later
+let num1 = '';
+let num2= '';
+let op = '';
 
+//Getting the element from HTML
+const btn = document.querySelectorAll('#btn');
+const inputField = document.getElementById('inputField');
+
+//Add basic calculator function to handle addition, subtraction, multiplication, and division
 function operate(num1, num2, op) {
+    num1 = Number(num1);
+    num2 = Number(num2)
     if(op === "-") {
         return num1 - num2;
     }
@@ -13,6 +21,45 @@ function operate(num1, num2, op) {
         return num1 * num2;
     }
     else if(op === "/") {
-        return num1 / num2;
+        return num2 !== 0 ? num1 / num2 : "Error"
     }
 }
+
+//Add event listener to all btns
+btn.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.value;
+        if(!isNaN(value)){
+            if(!op) {
+                num1 += value;
+            } else {
+                num2 += value;
+            }
+            inputField.value= num1 + op + num2;
+        }
+        else if (value === "+" || value === "-" || value === "*" || value === "/") {
+            if(num1) {
+                op = value;
+                inputField.value = num1 + op + num2;
+            }
+        }
+        else if(value === "=") {
+            if(num1 && num2 && op) {
+                const result = operate(num1, num2, op);
+                if(result === "Error") {
+                    inputField.value = "You can't do that pal"
+                } else {
+                    inputField.value = Math.round(result);
+                    num1 = result;
+                    num2 = '';
+                }
+            }
+        }
+        else if(value === "c") {
+            num1 =  '';
+            num2 = '';
+            op = '';
+            inputField.value = '';
+        }
+    })
+})
